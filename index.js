d8414de9c2f0eab898307f02066c5092ef798040
anthonyLuisSotoColling <govips@gmail.com>
2023-05-23 23:10:23 -0700

    functions labs

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    return Math.abs(pickUp - 42)
+
+}
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264
+
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance < 400) {
+        return 0
+    }
+    else if (distance >= 400 && distance <= 2000) {
+        return (distance - 400) * .02
+    }
+    else if (distance > 2001 && distance <= 2499) {
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+
+}
